3d757817f0f021f158e241c50e0cc48a63cfbc5a
Ethan Berk <Ethanwberk@gmail.com>
2023-02-24 09:57:46 -0700

    Last commit

--- src/routes/comp.route.ts
+++ src/routes/comp.route.ts
@@ -6,6 +6,9 @@ const router = express.Router();
 
 
 
+router.route("/events").get(async (req, res, next) => {
+    Competition2023.find().then((data) => {res.send(data)}).catch(next);
+});
 router.route("/event/:event").get((req, res, next) => {
         Promise.all([Competition2023.findById(req.params.event), getFromTBA("event/" + req.params.event)]).then(([event, tbaEvent]) => {
             // @ts-ignore
@@ -13,12 +16,27 @@ router.route("/event/:event").get((req, res, next) => {
         }).catch(next)
     }
 );
+router.route("/event/:event/matches/simple").get((req, res, next) => {
+    getFromTBA("event/" + req.params.event + "/matches/simple").then((matches) => {
+        res.send(matches);
+    }).catch(next)
+})
+
+router.route("/event/:event/updateMatches").patch((req, res, next) => {
+    getFromTBA("event/" + req.params.event + "/matches/keys").then((event) => {
+        event.map((key: any) => ({_id: key}))
+        console.log(event)
+        // Competition2023.updateOne({_id: req.params.event}, {matchScout: event}).then(() => {
+        //     res.send("Updated")
+        // }).catch(next)
+    }).catch(next)
+});
 
 
 
 /* POSTs*/
 router.route("/event").post((req, res, next) => {
-    console.log(req);
+    console.log(req.body);
     Competition2023.create(req.body)
         .then((event) => {
             res.send(event)
@@ -26,7 +44,23 @@ router.route("/event").post((req, res, next) => {
         .catch(next)
 });
 
-const getFromTBA = async (url: string) => {
+
+
+router.route("/event/:event/match/:match/team/:team").post( async (req, res, next) => {
+    const result = await Competition2023.updateOne(
+        {_id: req.params.event, "matchScout._id": req.params.match, "matchScout.teams._id": req.params.team},
+        {
+            $set: {
+                "matchScout.$.teams.$": req.body
+            }
+        }).then((event) => {
+            console.log(event);
+            console.log(event.matchedCount);
+    }).catch(next)
+
+});
+
+const getFromTBA = async (url: string) : Promise<[Object]> => {
     return got("https://www.thebluealliance.com/api/v3/" + url, {
         headers: {
             "X-TBA-Auth-Key": process.env.TBA_KEY
