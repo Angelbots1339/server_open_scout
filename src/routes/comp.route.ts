19e8d15eee789cbd8c6c297e258ceb4d09303b47
Ethan Berk <Ethanwberk@gmail.com>
2023-03-08 17:19:03 -0700

    added practice and fails

--- src/routes/comp.route.ts
+++ src/routes/comp.route.ts
@@ -5,170 +5,6 @@ import {Query} from "mongoose";
 import competition2023Model from "../models/Competition2023.model";
 
 const router = express.Router();
-
-
-router.route("/events").get(async (req, res, next) => {
-    Competition2023.find().then((data) => {
-        res.send(data)
-    }).catch(next);
-});
-router.route("/event/:event").get((req, res, next) => {
-        Promise.all([Competition2023.findById(req.params.event), getFromTBA("event/" + req.params.event)]).then(([event, tbaEvent]) => {
-            // @ts-ignore
-            res.send({...event?.toObject(), ...tbaEvent});
-        }).catch(next)
-    }
-);
-router.route("/event/:event/matches/simple").get((req, res, next) => {
-    getFromTBA("event/" + req.params.event + "/matches/simple").then((matches) => {
-        res.send(matches);
-    }).catch(next)
-})
-router.route("/event/:event/matches/keys").get((req, res, next) => {
-    getFromTBA("event/" + req.params.event + "/matches/keys").then((matches) => {
-        res.send(matches);
-    }).catch(next)
-})
-router.route("/event/:event/matches/flat").get((req, res, next) => {
-    Promise.all([competition2023Model.aggregate(getSummary(req.params.event)), getFromTBA("/event/" + req.params.event + "/teams")]).then(([matches, tbaTeams]) => {
-        let final = matches.map((match) => {
-            let nickname = tbaTeams.find((team: any) => {
-                // console.log(team.nickname);
-                return team.key === match._id;
-            }).nickname;
-
-            console.log(nickname);
-
-            // console.log({
-            //     ...match,
-            //     "nickname": nickname
-            // });
-
-            return {
-                ...match,
-                "nickname": nickname
-            }
-        })
-        console.log(final);
-        res.send(final);
-    });
-router.route("/event/:event/practiceMatches/flat").get((req, res, next) => {
-    competition2023Model.aggregate(getAllPracticeMatchSummery(req.params.event)).then((matches) => {
-        res.send(matches)
-    }).catch(next);
-})
-
-router.route("/event/:event/matches").get((req, res, next) => {
-    competition2023Model.aggregate(getAllMatches((req.params.event))).then((matches) => {
-        res.send(matches)
-    }).catch(next);
-})
-router.route("/event/:event/practiceMatches").get((req, res, next) => {
-    competition2023Model.aggregate(getAllPracticeMatches((req.params.event))).then((matches) => {
-        res.send(matches)
-    }).catch(next);
-})
-
-
-router.route("/event/:event/setMatches").patch((req, res, next) => {
-    getFromTBA("event/" + req.params.event + "/matches").then((event) => {
-
-        const mapped = event.map((event: any) => ({
-            _id: `${event.comp_level}${event.match_number}`
-            ,
-            teams: event.alliances.blue.team_keys.concat(event.alliances.red.team_keys).map((team: string) => ({
-                _id: team,
-                match: `${event.comp_level}${event.match_number}`
-            }))
-        }));
-
-        Competition2023.updateOne({_id: req.params.event}, {matchScout: mapped}, {new: true}).then(() => {
-            res.send("Updated")
-        }).catch(next)
-    }).catch(next)
-});
-
-
-/* POSTs*/
-router.route("/event").post((req, res, next) => {
-    console.log(req.body);
-    Competition2023.create(req.body)
-        .then((event) => {
-            res.send(event)
-        })
-        .catch(next)
-});
-
-router.route("/event/:event/teams").get((req, res, next) => {
-        Promise.all([getFromTBA("event/" + req.params['event'] + "/teams")]).then(([tbaEventTeams]) => {
-            // @ts-ignore
-            res.send({...tbaEventTeams});
-        }).catch(next);
-    }
-);
-router.route("/event/:event/team/:team/matches").get((req, res, next) => {
-        Competition2023.aggregate(getAllMatchesTeam(req.params.event, req.params.team)).then(
-            (matches) =>
-                res.send(matches)
-        ).catch(next)
-    }
-)
-router.route("/event/:event/team/:team/autos").get((req, res, next) => {
-        Competition2023.aggregate(getTeamAutos(req.params.event, req.params.team)).then(
-            (autos) =>
-                res.send(autos)
-        ).catch(next)
-    }
-)
-
-
-router.route("/event/:event/match/:match/team").post(async (req, res, next) => {
-    const result = await Competition2023.updateOne(
-        {_id: req.params.event},
-        {
-            $set: {
-                "matchScout.$[match].teams.$[team]": req.body
-            }
-        }, {
-            arrayFilters: [{
-                "match._id": req.params.match
-            }, {
-                "team._id": req.body._id
-            }]
-        }).then((event) => {
-        res.send(event)
-    }).catch(next)
-});
-router.route("/event/:event/practiceMatch").post(async (req, res, next) => {
-    const result = await Competition2023.updateOne(
-        {_id: req.params.event},
-        {
-            $push: {
-                "practiceMatches": req.body
-            }
-        }).then((event) => {
-        res.send(event)
-    }).catch(next)
-});
-
-
-router.route("/event/:event/match/:match/team/:team").get(async (req, res, next) => {
-    const result = await Competition2023.findOne(
-        {_id: req.params.event},
-        {
-            $get: "matchScout.$[match].teams.$[team]"
-        }, {
-            arrayFilters: [{
-                "match._id": req.params.match
-            }, {
-                "team._id": req.params.team
-            }]
-        }).then((event) => {
-        res.send(event)
-        console.log(event);
-    }).catch(next)
-});
-
 const getFromTBA = async (url: string): Promise<any> => {
     return got("https://www.thebluealliance.com/api/v3/" + url, {
         headers: {
@@ -176,7 +12,6 @@ const getFromTBA = async (url: string): Promise<any> => {
         }
     }).json();
 }
-
 const countCycle = (input: string, eq: string) => {
     return {
         $size: {
@@ -323,7 +158,7 @@ const convertArray = (array: Object) => {
         }
     }
 }
-const divdeZeroProtection = (num1: Object, num2: Object) => {
+const divideZeroProtection = (num1: Object, num2: Object) => {
     return {
         $cond:
             {
@@ -371,12 +206,12 @@ const addArrays = {
         stdEndGameContributedScore: "$stdEndGameContributedScore",
         endGameContributedScores: convertArray("$endGameContributedScores"),
         avgTotalCycles: "$avgTotalCycles",
-        percentOfPickedFromSub: divdeZeroProtection("$avgSubstationPickupCount", "$avgTotalCycles"),
-        percentOfPickedGround: divdeZeroProtection("$avgGroundPickupCount", "$avgTotalCycles"),
-        percentOfConesPickedTipped: divdeZeroProtection("$avgTipped", "$avgTotalConeCycles"),
-        percentCone: divdeZeroProtection("$avgTipped", "$avgTotalConeCycles"),
-        percentCube: divdeZeroProtection("$avgTotalCubeCycles", "$avgTotalCycles"),
-        percentFailed: divdeZeroProtection("$avgFailedCycles", "$avgTotalCycles")
+        percentOfPickedFromSub: divideZeroProtection("$avgSubstationPickupCount", "$avgTotalCycles"),
+        percentOfPickedGround: divideZeroProtection("$avgGroundPickupCount", "$avgTotalCycles"),
+        percentOfConesPickedTipped: divideZeroProtection("$avgTipped", "$avgTotalConeCycles"),
+        percentCone: divideZeroProtection("$avgTipped", "$avgTotalConeCycles"),
+        percentCube: divideZeroProtection("$avgTotalCubeCycles", "$avgTotalCycles"),
+        percentFailed: divideZeroProtection("$avgFailedCycles", "$avgTotalCycles")
     }
 }
 
@@ -473,35 +308,7 @@ const getAllPracticeMatchSummery = (comp: string) => {
     ]
 
 }
-const getSummery = (comp: string) => {
-    return [
-        {
-            $match: {
-                _id: comp
-            }
-        },
-        {
-            $unwind: "$matchScout"
-        },
-        {
-            $unwind: "$matchScout.teams"
-        },
-        {
-            $replaceRoot: {
-                newRoot: "$matchScout.teams"
-            }
-        },
-        {
-            $match: {
-                auto: {$exists: true}
-            }
-        },
-        countCycles,
-        getContributedScore,
-        groupTeams,
-        addArrays
-    ]
-}
+
 
 const getTeamAutos = (comp: string, team: string) => {
     return [
@@ -542,11 +349,204 @@ const getTeamAutos = (comp: string, team: string) => {
         }
     ]
 }
-
-const getMatchesFromEachTeam = async (): Promise<any> => {
-    return Competition2023.aggregate([]
-    );
+const getSummery = (comp: string) => {
+    return [
+        {
+            $match: {
+                _id: comp
+            }
+        },
+        {
+            $unwind: "$matchScout"
+        },
+        {
+            $unwind: "$matchScout.teams"
+        },
+        {
+            $replaceRoot: {
+                newRoot: "$matchScout.teams"
+            }
+        },
+        {
+            $match: {
+                auto: {$exists: true}
+            }
+        },
+        countCycles,
+        getContributedScore,
+        groupTeams,
+        addArrays
+    ]
 }
 
 
+router.route("/events").get(async (req, res, next) => {
+    Competition2023.find().then((data) => {
+        res.send(data)
+    }).catch(next);
+});
+router.route("/event/:event").get((req, res, next) => {
+        Promise.all([Competition2023.findById(req.params.event), getFromTBA("event/" + req.params.event)]).then(([event, tbaEvent]) => {
+            // @ts-ignore
+            res.send({...event?.toObject(), ...tbaEvent});
+        }).catch(next)
+    }
+);
+router.route("/event/:event/matches/simple").get((req, res, next) => {
+    getFromTBA("event/" + req.params.event + "/matches/simple").then((matches) => {
+        res.send(matches);
+    }).catch(next)
+})
+router.route("/event/:event/matches/keys").get((req, res, next) => {
+    getFromTBA("event/" + req.params.event + "/matches/keys").then((matches) => {
+        res.send(matches);
+    }).catch(next)
+})
+router.route("/event/:event/matches/flat").get((req, res, next) => {
+    Promise.all([competition2023Model.aggregate(getSummery(req.params.event)), getFromTBA("/event/" + req.params.event + "/teams")]).then(([matches, tbaTeams]) => {
+        let final = matches.map((match) => {
+            let nickname = tbaTeams.find((team: any) => {
+                // console.log(team.nickname);
+                return team.key === match._id;
+            }).nickname;
+
+            console.log(nickname);
+
+            // console.log({
+            //     ...match,
+            //     "nickname": nickname
+            // });
+
+            return {
+                ...match,
+                "nickname": nickname
+            }
+        })
+        console.log(final);
+        res.send(final);
+    });
+    router.route("/event/:event/practiceMatches/flat").get((req, res, next) => {
+        competition2023Model.aggregate(getAllPracticeMatchSummery(req.params.event)).then((matches) => {
+            res.send(matches)
+        }).catch(next);
+    })
+
+    router.route("/event/:event/matches").get((req, res, next) => {
+        competition2023Model.aggregate(getAllMatches((req.params.event))).then((matches) => {
+            res.send(matches)
+        }).catch(next);
+    })
+    router.route("/event/:event/practiceMatches").get((req, res, next) => {
+        competition2023Model.aggregate(getAllPracticeMatches((req.params.event))).then((matches) => {
+            res.send(matches)
+        }).catch(next);
+    })
+
+
+    router.route("/event/:event/setMatches").patch((req, res, next) => {
+        getFromTBA("event/" + req.params.event + "/matches").then((event) => {
+
+            const mapped = event.map((event: any) => ({
+                _id: `${event.comp_level}${event.match_number}`
+                ,
+                teams: event.alliances.blue.team_keys.concat(event.alliances.red.team_keys).map((team: string) => ({
+                    _id: team,
+                    match: `${event.comp_level}${event.match_number}`
+                }))
+            }));
+
+            Competition2023.updateOne({_id: req.params.event}, {matchScout: mapped}, {new: true}).then(() => {
+                res.send("Updated")
+            }).catch(next)
+        }).catch(next)
+    });
+
+
+    /* POSTs*/
+    router.route("/event").post((req, res, next) => {
+        console.log(req.body);
+        Competition2023.create(req.body)
+            .then((event) => {
+                res.send(event)
+            })
+            .catch(next)
+    });
+
+    router.route("/event/:event/teams").get((req, res, next) => {
+            Promise.all([getFromTBA("event/" + req.params['event'] + "/teams")]).then(([tbaEventTeams]) => {
+                // @ts-ignore
+                res.send({...tbaEventTeams});
+            }).catch(next);
+        }
+    );
+    router.route("/event/:event/team/:team/matches").get((req, res, next) => {
+            Competition2023.aggregate(getAllMatchesTeam(req.params.event, req.params.team)).then(
+                (matches) =>
+                    res.send(matches)
+            ).catch(next)
+        }
+    )
+    router.route("/event/:event/team/:team/autos").get((req, res, next) => {
+            Competition2023.aggregate(getTeamAutos(req.params.event, req.params.team)).then(
+                (autos) =>
+                    res.send(autos)
+            ).catch(next)
+        }
+    )
+
+
+    router.route("/event/:event/match/:match/team").post(async (req, res, next) => {
+        const result = await Competition2023.updateOne(
+            {_id: req.params.event},
+            {
+                $set: {
+                    "matchScout.$[match].teams.$[team]": req.body
+                }
+            }, {
+                arrayFilters: [{
+                    "match._id": req.params.match
+                }, {
+                    "team._id": req.body._id
+                }]
+            }).then((event) => {
+            res.send(event)
+        }).catch(next)
+    });
+    router.route("/event/:event/practiceMatch").post(async (req, res, next) => {
+        const result = await Competition2023.updateOne(
+            {_id: req.params.event},
+            {
+                $push: {
+                    "practiceMatches": req.body
+                }
+            }).then((event) => {
+            res.send(event)
+        }).catch(next)
+    });
+
+
+    router.route("/event/:event/match/:match/team/:team").get(async (req, res, next) => {
+        const result = await Competition2023.findOne(
+            {_id: req.params.event},
+            {
+                $get: "matchScout.$[match].teams.$[team]"
+            }, {
+                arrayFilters: [{
+                    "match._id": req.params.match
+                }, {
+                    "team._id": req.params.team
+                }]
+            }).then((event) => {
+            res.send(event)
+            console.log(event);
+        }).catch(next)
+    });
+
+
+    const getMatchesFromEachTeam = async (): Promise<any> => {
+        return Competition2023.aggregate([]
+        );
+    }
+
+})
 export default router;
