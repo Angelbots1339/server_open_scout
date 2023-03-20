cb5dedc2049582926201276370de9f283ac12283
Ethan Berk <Ethanwberk@gmail.com>
2023-03-20 09:22:45 -0600

    fix request and add place height%

--- src/routes/comp.route.ts
+++ src/routes/comp.route.ts
@@ -120,6 +120,7 @@ const countCycles = {
         topCount: countCycle("placement", "top"),
         midCount: countCycle("placement", "mid"),
         hybridCount: countCycle("placement", "hybrid"),
+        failedCount: countCycle("placement", "fail"),
         groundPickupCount: countCycle("pickup", "ground"),
         tippedPickupCount: countCycle("pickup", "tipped"),
         substationPickupCount: countCycle("pickup", "substation"),
@@ -160,62 +161,68 @@ const convertArray = (array: Object) => {
 }
 const divideZeroProtection = (num1: Object, num2: Object) => {
     return {
-        $cond:
+        $round: [
             {
-                if: {$eq: [num2, 0]},
-                then: 0,
-                else: {$divide: [num1, num2]}
-            }
+                $cond:
+                    {
+                        if: {$eq: [num2, 0]},
+                        then: 0,
+                        else: {$divide: [num1, num2]}
+                    }
+            },
+            2
+        ]
     }
+
 }
 const groupTeams = {
     $group: {
         _id: "$_id",
+        matchCount: {$sum: 1},
+        totalCycles: {$sum: "$totalCycles"},
+        avgCycles: {$avg: "$totalCycles"},
         avgContributedScore: {$avg: "$contributedScore"},
-        stdContributedScore: {$stdDevPop: "$contributedScore"},
         contributedScores: {$push: "$contributedScore"},
         avgAutoContributedScore: {$avg: "$autoScore"},
-        stdAutoContributedScore: {$stdDevPop: "$autoScore"},
         autoContributedScores: {$push: "$autoScore"},
         avgTeleopContributedScore: {$avg: "$teleopScore"},
-        stdTeleopContributedScore: {$stdDevPop: "$teleopScore"},
         teleopContributedScores: {$push: "$teleopScore"},
         avgEndGameContributedScore: {$avg: "$endGameScore"},
-        stdEndGameContributedScore: {$stdDevPop: "$endGameScore"},
         endGameContributedScores: {$push: "$endGameScore"},
-        avgTotalCycles: {$avg: "$totalCycles"},
-        avgTotalConeCycles: {$avg: "$totalCone"},
-        avgTotalCubeCycles: {$avg: "$totalCube"},
-        avgSubstationPickupCount: {$avg: "$substationPickupCount"},
-        avgGroundPickupCount: {$avg: "$groundPickupCount"},
-        avgTipped: {$avg: "$tippedPickupCount"}
+        totalConeCycles: {$sum: "$totalCone"},
+        totalCubeCycles: {$sum: "$totalCube"},
+        totalHighCycles: {$sum: "$topCount"},
+        totalMidCycles: {$sum: "$midCount"},
+        totalHybridCycles: {$sum: "$hybridCount"},
+        totalFailedCycles: {$sum: "$failedCount"},
+        totalSubstationPickupCount: {$sum: "$substationPickupCount"},
+        totalGroundPickupCount: {$sum: "$groundPickupCount"},
+        totalTipped: {$sum: "$tippedPickupCount"}
     }
 }
 const addArrays = {
     $project: {
         avgContributedScore: "$avgContributedScore",
-        stdContributedScore: "$stdContributedScore",
-        contributedScores: convertArray("$contributedScores"),
         avgAutoContributedScore: "$avgAutoContributedScore",
-        stdAutoContributedScore: "$stdAutoContributedScore",
-        autoContributedScores: convertArray("$autoContributedScores"),
         avgTeleopContributedScore: "$avgTeleopContributedScore",
-        stdTeleopContributedScore: "$stdTeleopContributedScore",
-        teleopContributedScores: convertArray("$teleopContributedScores"),
         avgEndGameContributedScore: "$avgEndGameContributedScore",
-        stdEndGameContributedScore: "$stdEndGameContributedScore",
+        contributedScores: convertArray("$contributedScores"),
+        autoContributedScores: convertArray("$autoContributedScores"),
+        teleopContributedScores: convertArray("$teleopContributedScores"),
         endGameContributedScores: convertArray("$endGameContributedScores"),
-        avgTotalCycles: "$avgTotalCycles",
-        percentOfPickedFromSub: divideZeroProtection("$avgSubstationPickupCount", "$avgTotalCycles"),
-        percentOfPickedGround: divideZeroProtection("$avgGroundPickupCount", "$avgTotalCycles"),
-        percentOfConesPickedTipped: divideZeroProtection("$avgTipped", "$avgTotalConeCycles"),
-        percentCone: divideZeroProtection("$avgTipped", "$avgTotalConeCycles"),
-        percentCube: divideZeroProtection("$avgTotalCubeCycles", "$avgTotalCycles"),
-        percentFailed: divideZeroProtection("$avgFailedCycles", "$avgTotalCycles")
+        avgTotalCycles: "$avgCycles",
+        matchCount: "$matchCount",
+        percentHigh: divideZeroProtection("$totalHighCycles", "$totalCycles"),
+        percentMid: divideZeroProtection("$totalMidCycles", "$totalCycles"),
+        percentHybrid: divideZeroProtection("$totalHybridCycles", "$totalCycles"),
+        percentCone: divideZeroProtection("$totalConeCycles", "$totalCycles"),
+        percentCube: divideZeroProtection("$totalCubeCycles", "$totalCycles"),
+        percentOfPickedFromSub: divideZeroProtection("$totalSubstationPickupCount", "$totalCycles"),
+        percentOfPickedGround: divideZeroProtection("$totalGroundPickupCount", "$totalCycles"),
+        percentOfConesPickedTipped: divideZeroProtection("$totalTipped", "$totalConeCycles"),
+        percentFailed: divideZeroProtection("$totalFailedCycles", "$totalCycles")
     }
 }
-
-
 
 
 const getAllMatchesTeam = (comp: string, team: string) => {
@@ -230,6 +237,11 @@ const getAllMatchesTeam = (comp: string, team: string) => {
         },
         {
             $unwind: "$matchScout.teams"
+        },
+        {
+            $addFields: {
+                "matchScout.teams.match" : "$matchScout._id"
+            }
         },
         {
             $replaceRoot: {
@@ -261,6 +273,11 @@ const getAllMatches = (comp: string) => {
         },
         {
             $unwind: "$matchScout.teams"
+        },
+        {
+            $addFields: {
+                "matchScout.teams.match" : "$matchScout._id"
+            }
         },
         {
             $replaceRoot: {
@@ -416,7 +433,7 @@ router.route("/event/:event/matches/flat").get((req, res, next) => {
                 return team.key === match._id;
             });
 
-            if(nickname != undefined) {
+            if (nickname != undefined) {
                 nickname = nickname.nickname;
             }
 
@@ -429,145 +446,144 @@ router.route("/event/:event/matches/flat").get((req, res, next) => {
     });
 })
 
-    router.route("/event/:event/practiceMatches/flat").get((req, res, next) => {
-        Promise.all([competition2023Model.aggregate(getAllPracticeMatchSummary(req.params.event)), getFromTBA("/event/" + req.params.event + "/teams")]).then(([matches, tbaTeams]) => {
-            let final = matches.map((match) => {
-                let nickname = tbaTeams.find((team: any) => {
-                    // console.log(team.nickname);
-                    return team.key === match._id;
-                });
+router.route("/event/:event/practiceMatches/flat").get((req, res, next) => {
+    Promise.all([competition2023Model.aggregate(getAllPracticeMatchSummary(req.params.event)), getFromTBA("/event/" + req.params.event + "/teams")]).then(([matches, tbaTeams]) => {
+        let final = matches.map((match) => {
+            let nickname = tbaTeams.find((team: any) => {
+                // console.log(team.nickname);
+                return team.key === match._id;
+            });
 
-                if(nickname != undefined) {
-                    nickname = nickname.nickname;
-                }
+            if (nickname != undefined) {
+                nickname = nickname.nickname;
+            }
 
-                console.log(nickname);
-                return {
-                    ...match,
-                    "nickname": nickname
-                }
-            })
-            res.send(final);
-        });
-    })
+            console.log(nickname);
+            return {
+                ...match,
+                "nickname": nickname
+            }
+        })
+        res.send(final);
+    });
+})
 
-    router.route("/event/:event/matches").get((req, res, next) => {
-        competition2023Model.aggregate(getAllMatches((req.params.event))).then((matches) => {
-            res.send(matches)
+router.route("/event/:event/matches").get((req, res, next) => {
+    competition2023Model.aggregate(getAllMatches((req.params.event))).then((matches) => {
+        res.send(matches)
+    }).catch(next);
+})
+router.route("/event/:event/practiceMatches").get((req, res, next) => {
+    competition2023Model.aggregate(getAllPracticeMatches((req.params.event))).then((matches) => {
+        res.send(matches)
+    }).catch(next);
+})
+
+
+router.route("/event/:event/setMatches").patch((req, res, next) => {
+    console.log("here")
+    getFromTBA("event/" + req.params.event + "/matches").then((event) => {
+        const mapped = event.map((event: any) => ({
+            _id: `${event.comp_level}${event.match_number}`
+            ,
+            teams: event.alliances.blue.team_keys.concat(event.alliances.red.team_keys).map((team: string) => ({
+                _id: team,
+                match: `${event.comp_level}${event.match_number}`
+            }))
+        }));
+
+        Competition2023.updateOne({_id: req.params.event}, {matchScout: mapped}, {new: true}).then(() => {
+            res.send("Updated")
+        }).catch(next)
+    }).catch(next)
+});
+
+
+/* POSTs*/
+router.route("/event").post((req, res, next) => {
+    console.log(req.body);
+    Competition2023.create(req.body)
+        .then((event) => {
+            res.send(event)
+        })
+        .catch(next)
+});
+
+router.route("/event/:event/teams").get((req, res, next) => {
+        Promise.all([getFromTBA("event/" + req.params['event'] + "/teams")]).then(([tbaEventTeams]) => {
+            // @ts-ignore
+            res.send({...tbaEventTeams});
         }).catch(next);
-    })
-    router.route("/event/:event/practiceMatches").get((req, res, next) => {
-        competition2023Model.aggregate(getAllPracticeMatches((req.params.event))).then((matches) => {
-            res.send(matches)
-        }).catch(next);
-    })
-
-
-    router.route("/event/:event/setMatches").patch((req, res, next) => {
-        getFromTBA("event/" + req.params.event + "/matches").then((event) => {
-
-            const mapped = event.map((event: any) => ({
-                _id: `${event.comp_level}${event.match_number}`
-                ,
-                teams: event.alliances.blue.team_keys.concat(event.alliances.red.team_keys).map((team: string) => ({
-                    _id: team,
-                    match: `${event.comp_level}${event.match_number}`
-                }))
-            }));
-
-            Competition2023.updateOne({_id: req.params.event}, {matchScout: mapped}, {new: true}).then(() => {
-                res.send("Updated")
-            }).catch(next)
-        }).catch(next)
-    });
-
-
-    /* POSTs*/
-    router.route("/event").post((req, res, next) => {
-        console.log(req.body);
-        Competition2023.create(req.body)
-            .then((event) => {
-                res.send(event)
-            })
-            .catch(next)
-    });
-
-    router.route("/event/:event/teams").get((req, res, next) => {
-            Promise.all([getFromTBA("event/" + req.params['event'] + "/teams")]).then(([tbaEventTeams]) => {
-                // @ts-ignore
-                res.send({...tbaEventTeams});
-            }).catch(next);
-        }
-    );
-    router.route("/event/:event/team/:team/matches").get((req, res, next) => {
-            Competition2023.aggregate(getAllMatchesTeam(req.params.event, req.params.team)).then(
-                (matches) =>
-                    res.send(matches)
-            ).catch(next)
-        }
-    )
-    router.route("/event/:event/team/:team/autos").get((req, res, next) => {
-            Competition2023.aggregate(getTeamAutos(req.params.event, req.params.team)).then(
-                (autos) =>
-                    res.send(autos)
-            ).catch(next)
-        }
-    )
-
-
-    router.route("/event/:event/match/:match/team").post(async (req, res, next) => {
-        const result = await Competition2023.updateOne(
-            {_id: req.params.event},
-            {
-                $set: {
-                    "matchScout.$[match].teams.$[team]": req.body
-                }
-            }, {
-                arrayFilters: [{
-                    "match._id": req.params.match
-                }, {
-                    "team._id": req.body._id
-                }]
-            }).then((event) => {
-            res.send(event)
-        }).catch(next)
-    });
-    router.route("/event/:event/practiceMatch").post(async (req, res, next) => {
-        const result = await Competition2023.updateOne(
-            {_id: req.params.event},
-            {
-                $push: {
-                    "practiceMatches": req.body
-                }
-            }).then((event) => {
-            res.send(event)
-        }).catch(next)
-    });
-
-
-    router.route("/event/:event/match/:match/team/:team").get(async (req, res, next) => {
-        const result = await Competition2023.findOne(
-            {_id: req.params.event},
-            {
-                $get: "matchScout.$[match].teams.$[team]"
-            }, {
-                arrayFilters: [{
-                    "match._id": req.params.match
-                }, {
-                    "team._id": req.params.team
-                }]
-            }).then((event) => {
-            res.send(event)
-            console.log(event);
-        }).catch(next)
-    });
-
-
-    const getMatchesFromEachTeam = async (): Promise<any> => {
-        return Competition2023.aggregate([]
-        );
     }
+);
+router.route("/event/:event/team/:team/matches").get((req, res, next) => {
+        Competition2023.aggregate(getAllMatchesTeam(req.params.event, req.params.team)).then(
+            (matches) =>
+                res.send(matches)
+        ).catch(next)
+    }
+)
+router.route("/event/:event/team/:team/autos").get((req, res, next) => {
+        Competition2023.aggregate(getTeamAutos(req.params.event, req.params.team)).then(
+            (autos) =>
+                res.send(autos)
+        ).catch(next)
+    }
+)
 
+
+router.route("/event/:event/match/:match/team").post(async (req, res, next) => {
+    const result = await Competition2023.updateOne(
+        {_id: req.params.event},
+        {
+            $set: {
+                "matchScout.$[match].teams.$[team]": req.body
+            }
+        }, {
+            arrayFilters: [{
+                "match._id": req.params.match
+            }, {
+                "team._id": req.body._id
+            }]
+        }).then((event) => {
+        res.send(event)
+    }).catch(next)
+});
+router.route("/event/:event/practiceMatch").post(async (req, res, next) => {
+    const result = await Competition2023.updateOne(
+        {_id: req.params.event},
+        {
+            $push: {
+                "practiceMatches": req.body
+            }
+        }).then((event) => {
+        res.send(event)
+    }).catch(next)
+});
+
+
+router.route("/event/:event/match/:match/team/:team").get(async (req, res, next) => {
+    const result = await Competition2023.findOne(
+        {_id: req.params.event},
+        {
+            $get: "matchScout.$[match].teams.$[team]"
+        }, {
+            arrayFilters: [{
+                "match._id": req.params.match
+            }, {
+                "team._id": req.params.team
+            }]
+        }).then((event) => {
+        res.send(event)
+        console.log(event);
+    }).catch(next)
+});
+
+
+const getMatchesFromEachTeam = async (): Promise<any> => {
+    return Competition2023.aggregate([]
+    );
+}
 
 
 export default router;
